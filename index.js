40c1fa0880b0a9b1382f7744990febdb16945546
Dijaa <javan_dhein@outlook.com>
2022-12-23 11:10:55 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/public/index.html");
+});
+
+server.listen(3000, () => {
+    console.log("listening on *:3000");
+});
+
+io.on("connection", (socket) => {
+    console.log("a user connected");
+    socket.on("disconnect", () => {
+        console.log("user disconnected");
+    });
+});
+
+io.on("connection", (socket) => {
+    socket.on("chat message", (msg) => {
+        io.emit("chat message", msg);
+        console.log("message: " + msg);
+    });
+});
