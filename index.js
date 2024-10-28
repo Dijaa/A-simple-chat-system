ba9f16848b4d7dc4f7de06e3992d9c469a8a089d
Javan <javan_dhein@outlook.com>
2024-10-28 10:56:05 -0300

    Refactor: Adicionar suporte a salas de chat

--- index.js
+++ index.js
@@ -5,25 +5,32 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
+app.use(express.static(__dirname + '/public'));
+
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/public/index.html");
 });
 
 const port = process.env.PORT || 3000;
 server.listen(port, () => {
-    console.log(`listening on *:${port}`);
+    console.log(`Servidor rodando na porta ${port}`);
 });
 
 io.on("connection", (socket) => {
-    console.log("a user connected");
-    socket.on("disconnect", () => {
-        console.log("user disconnected");
-    });
-});
+    console.log("Um usuário se conectou");
 
-io.on("connection", (socket) => {
-    socket.on("chat message", (msg) => {
-        io.emit("chat message", msg);
-        console.log("message: " + msg);
+    socket.on('join room', (room) => {
+        socket.join(room);
+        console.log(`Usuário entrou na sala: ${room}`);
+
+        socket.on("chat message", (data) => {
+            io.to(data.room).emit("chat message", data.message);
+            console.log(`Mensagem para ${data.room}: ${data.message}`);
+        });
+
+        socket.on("disconnect", () => {
+            console.log("Usuário desconectado");
+            socket.leave(room);
+        });
     });
 });
